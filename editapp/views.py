b5b4afb0dd64a5eb5d257cbaa8fa6bda8218e78e
Alexander Popelyuk <alexander.popelyuk@gmail.com>
2018-10-26 16:08:17 +0300

    nearly works, error with post handler response
    __init__() got an unexpected keyword argument 'request'
    Exception Location:	C:\Users\Alexander\AppData\Local\Programs\Python\Python37-32\lib\site-packages\django\http\response.py in __init__, line 289

--- editapp/views.py
+++ editapp/views.py
@@ -2,17 +2,20 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.forms.models import ModelForm
 from django.shortcuts import HttpResponse, Http404
+from django.http.response import HttpResponseBadRequest, HttpResponseRedirect
 from editapp.models import Entry
-from django.template.backends.django import Template
+from django.template import Template, RequestContext
 
+class HttpConflict(HttpResponse):
+  status_code = 409
 
 class EditForm(ModelForm):
   class Meta:
+    model = Entry
     fields = ['title', 'text']
 
+
 class EditView(TemplateView):
-  
-  template_name = "edit-page.html"
   
   def get(self, request, *args, **kwargs):
     action = 'view'
@@ -20,19 +23,35 @@ class EditView(TemplateView):
       action = request.GET['action']
     
     if action == 'create':
-      self.template_name = "edit-page.html"
-      
-      form = EditForm()
       if Entry.objects.filter(title__exact=kwargs['title']).exists():
-        raise Http404
-      template = Template(entry.text)
+        return HttpConflict()
+      form = EditForm(initial={'title': kwargs['title']})
+      self.template_name = "edit-page.html"
       self.extra_context = {
-        'entry': {
-          'form': EditForm()
+        'page': {
+          'title': kwargs['title'],
+          'form': form
+        },
+        'form': {
+          'action': 'create',
         }
       }
     elif action == 'edit':
-      entry = Entry.objects.get(title__exact=kwargs['title'])
+      try:
+        entry = Entry.objects.get(title__exact=kwargs['title'])
+      except Entry.DoesNotExist:
+        raise Http404
+      form = EditForm(initial={'title': entry.title, 'text': entry.text})
+      self.template_name = "edit-page.html"
+      self.extra_context = {
+        'page': {
+          'title': entry.title,
+          'form': form
+        },
+        'form': {
+          'action': 'update',
+        }
+      }
     else:
       self.template_name = "view-page.html"
       try:
@@ -40,21 +59,53 @@ class EditView(TemplateView):
       except Entry.DoesNotExist:
         raise Http404
       template = Template(entry.text)
+      context = RequestContext(request)
       self.extra_context = {
-        'entry': {
-          'text': template.render(request=request)
+        'page': {
+          'title': entry.title,
+          'text': template.render(context)
         }
       }
       
-    return super().get(request=request, *args, **kwargs)
+    return super().get(request, *args, **kwargs)
   
   def post(self, request, *args, **kwargs):
     self.template_name = "edit-page.html"
-    return HttpResponse('ok')
-  
-  def get_context_data(self, **kwargs):
-    return {
+    form = EditForm(request.POST)
+    action = 'update'
+    if 'action' in request.POST:
+      action = request.POST['action']
+    
+    if form.is_valid():
+      try:
+        entry = Entry.objects.get(title__exact=kwargs['title'])
+      except Entry.DoesNotExist:
+        entry = None
+      if action == 'create':
+        if entry is not None:
+          self.response_class = HttpConflict
+        entry = Entry()
+      elif entry is None:
+        raise Http404
+      
+      entry.title = form.cleaned_data['title']
+      entry.text = form.cleaned_data['text']
+      entry.save()
+      base = '://'.join((request.get_host(), request.scheme))
+      url = '/'.join((base, entry.title))
+      return HttpResponseRedirect(url)
+    
+    else:
+      self.response_class = HttpResponseBadRequest
+    
+    self.extra_context = {
       'page': {
-        'title': "Edit page title"
+        'title': kwargs['title'],
+        'form': form
+      },
+      'form': {
+        'action': action,
       }
     }
+    
+    return super().get(request, *args, **kwargs)
